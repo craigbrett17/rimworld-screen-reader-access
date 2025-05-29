52243f7b78606b8eda17a48244ce86c54cc46679
Craig Brett <craigbrett17@aol.com>
2025-05-29 01:50:18 +0100

    Initial test mod

---
+++ Source/ScreenReaderAccess/ScreenReaderAccessCore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace ScreenReaderAccess
+{
+    public class ScreenReaderAccess : Mod
+    {
+        public ScreenReaderAccess(ModContentPack content) : base(content)
+        {
+            // Initialization code can go here if needed
+            // log out to the dev console that the mod has been loaded
+            Log.Message("ScreenReaderAccess mod has been loaded successfully.");
+        }
+    }
+}
