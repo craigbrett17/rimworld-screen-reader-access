ec2d5a20be62101cc72a9cc508934947940b18bf
Craig Brett <craigbrett17@aol.com>
2025-08-07 01:39:34 +0100

    Revert "Solve double-announcement of letters"
    This reverts commit cdd1cd5ad6744e806a097027ada7029beec60da8.

--- Source/ScreenReaderAccess/Patches/LetterMaker_MakeLetter_Patch.cs
+++ Source/ScreenReaderAccess/Patches/LetterMaker_MakeLetter_Patch.cs
@@ -26,4 +26,35 @@ namespace ScreenReaderAccess.Patches
             }
         }
     }
+
+    // Attribute-based Harmony patch for LetterMaker.MakeLetter (with label and text parameters)
+    [HarmonyPatch(typeof(Verse.LetterMaker), "MakeLetter", typeof(TaggedString), typeof(TaggedString), typeof(LetterDef), typeof(Faction), typeof(Quest))]
+    public class LetterMaker_MakeLetter_LabelText_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(Verse.Letter __result)
+        {
+            if (__result != null)
+            {
+                var letterInfo = new LetterDto { Label = __result.Label.ToString() };
+                ScreenReaderAccess.EventBusInstance?.RaiseEvent(new MakeLetterEvent(letterInfo));
+            }
+        }
+    }
+
+    // Attribute-based Harmony patch for LetterMaker.MakeLetter (with label, text, and look targets parameters)
+    // public static ChoiceLetter MakeLetter(TaggedString label, TaggedString text, LetterDef def, LookTargets lookTargets, Faction relatedFaction = null, Quest quest = null, List<ThingDef> hyperlinkThingDefs = null);
+    [HarmonyPatch(typeof(Verse.LetterMaker), "MakeLetter", typeof(TaggedString), typeof(TaggedString), typeof(LetterDef), typeof(LookTargets), typeof(Faction), typeof(Quest), typeof(List<ThingDef>))]
+    public class LetterMaker_MakeLetter_LabelTextLookTargets_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(Verse.Letter __result)
+        {
+            if (__result != null)
+            {
+                var letterInfo = new LetterDto { Label = __result.Label.ToString() };
+                ScreenReaderAccess.EventBusInstance?.RaiseEvent(new MakeLetterEvent(letterInfo));
+            }
+        }
+    }
 }
